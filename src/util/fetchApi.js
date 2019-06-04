4f96cc8d429b48fe21716fd7e743a55d80cb0e1d
shike.miao@gmail.com <shike.miao@gmail.com>
2019-06-04 00:16:46 -0500

    proxy ok

--- src/util/fetchApi.js
+++ src/util/fetchApi.js
@@ -1,13 +1,12 @@
 import axios from "axios";
 
-const baseUrl = "https://api.douban.com/";
 const apiKey = "0b2bdeda43b5688921839c8ecb20399b";
 
 const fetchApi = {
     RestApi: (url, method) => {
         axios({
             method,
-            url:baseUrl+url,
+            url,
             params: {
                 apikey:apiKey
             }
